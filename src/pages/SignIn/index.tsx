e557e873d9bd9905a2d078fd3f71f15e3939ca5b
jonascr86 <jonascr86@gmail.com>
2020-06-24 20:05:08 -0300

    retirada de commit

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -19,8 +19,6 @@ const SigIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const { user, signIn } = useContext(AuthContext);
 
-  console.log(user);
-
   const handleSubmit = useCallback(async (data: SignInFormData) => {
     formRef.current?.setErrors({});
     try {
